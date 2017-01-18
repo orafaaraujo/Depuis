fcbb22d46750cf292adf6f87d31244d1114c063c
orafaaraujo <orafaaraujo@gmail.com>
2017-01-18 01:47:06 -0200

    Moving packages of MainActivity

---
+++ app/src/main/java/com/orafaaraujo/depuis/presentation/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.orafaaraujo.depuis.presentation.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.orafaaraujo.depuis.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
