6cd968059a2957625c9f4fdd817286164f60f19e
Rafael de Araujo <rafaelar@motorola.com>
2017-01-20 12:49:17 -0200

    Updating cardview.

--- app/src/main/java/com/orafaaraujo/depuis/viewModel/FactViewModel.java
+++ app/src/main/java/com/orafaaraujo/depuis/viewModel/FactViewModel.java
@@ -4,6 +4,8 @@ import android.databinding.BaseObservable;
 
 import com.orafaaraujo.depuis.model.Fact;
 
+import java.util.Date;
+
 /**
  * Created by rafael on 18/01/17.
  */
@@ -19,4 +21,23 @@ public class FactViewModel extends BaseObservable {
     public String getFactTitle() {
         return fact.title();
     }
+
+    public String getFactComment() {
+        return fact.comment();
+    }
+
+    public String getFactBegin() {
+        return new Date(fact.timestamp()).toString();
+    }
+
+    public String getFactCurrentTime() {
+        return new Date(fact.timestamp()).toString();
+    }
+
+
+    public boolean getFactCount() {
+        return fact.count();
+    }
+
+
 }
