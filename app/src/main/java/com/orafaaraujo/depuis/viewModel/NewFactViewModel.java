3c1a1e3c35e9eb143103d1bb69f4f99daa7557d8
Rafael de Araujo <rafaelar@motorola.com>
2017-01-25 17:36:02 -0200

    Start ViewModel of NewFact

---
+++ app/src/main/java/com/orafaaraujo/depuis/viewModel/NewFactViewModel.java
@@ -0,0 +1,10 @@
+package com.orafaaraujo.depuis.viewModel;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by rafael on 25/01/17.
+ */
+
+public class NewFactViewModel extends BaseObservable {
+}
