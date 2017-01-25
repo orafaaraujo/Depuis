d8fc74a6b4aa83b13b5a3786e7b90c9b214b01d5
Rafael de Araujo <rafaelar@motorola.com>
2017-01-25 17:36:21 -0200

    Start ViewModel of NewFact

--- app/src/main/java/com/orafaaraujo/depuis/viewModel/NewFactViewModel.java
+++ app/src/main/java/com/orafaaraujo/depuis/viewModel/NewFactViewModel.java
@@ -2,9 +2,32 @@ package com.orafaaraujo.depuis.viewModel;
 
 import android.databinding.BaseObservable;
 
+import com.orafaaraujo.depuis.helper.DateTimeHelper;
+
+import java.util.Calendar;
+
+import javax.inject.Inject;
+
 /**
  * Created by rafael on 25/01/17.
  */
 
 public class NewFactViewModel extends BaseObservable {
+
+    @Inject
+    DateTimeHelper mDateTimeHelper;
+
+    private long mTimeInMillis;
+
+    public NewFactViewModel() {
+        mTimeInMillis = mDateTimeHelper.getCleanTime(Calendar.getInstance());
+    }
+
+    public String getDate() {
+        return mDateTimeHelper.getDate(mTimeInMillis);
+    }
+
+    public String getTime() {
+        return mDateTimeHelper.getTime(mTimeInMillis);
+    }
 }
