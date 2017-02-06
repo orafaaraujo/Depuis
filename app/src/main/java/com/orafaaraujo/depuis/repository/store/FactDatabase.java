8f09a29da5a6e83ff99336a507e8fd400b48d4c4
Rafael de Araujo <rafaelar@motorola.com>
2017-02-06 13:26:55 -0200

    Database interface

--- app/src/main/java/com/orafaaraujo/depuis/repository/store/FactDatabase.java
+++ app/src/main/java/com/orafaaraujo/depuis/repository/store/FactDatabase.java
@@ -1,8 +1,21 @@
 package com.orafaaraujo.depuis.repository.store;
 
+import com.orafaaraujo.depuis.repository.FactEntity;
+
+import java.util.List;
+
 /**
  * Created by rafael on 06/02/17.
  */
 
-public class FactDatabase {
+public interface FactDatabase {
+
+    boolean saveFact();
+
+    List<FactEntity> fetchAll();
+
+    FactEntity fetchFact(int id);
+
+    boolean deleteFact();
+
 }
