6ded9a1c77140a9498e5fae9c5ee88a40285cc2f
orafaaraujo <orafaaraujo@gmail.com>
2018-06-03 17:47:43 -0300

    Moving activity to new package.

--- app/src/main/java/com/orafaaraujo/depuis/features/main/MainActivity.kt
+++ app/src/main/java/com/orafaaraujo/depuis/features/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.orafaaraujo.depuis
+package com.orafaaraujo.depuis.features.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.orafaaraujo.depuis.R
 
 class MainActivity : AppCompatActivity() {
 
